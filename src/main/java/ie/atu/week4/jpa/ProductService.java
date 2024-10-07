2512fa0e071990e4d01e9bc5e7e0fc663d24552d
Dan Koskiranta <G00397054@atu.ie>
2024-10-07 14:45:13 +0100

    Modified code

--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -1,6 +1,7 @@
 package ie.atu.week4.jpa;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,25 @@ public class ProductService {
 
     public List<Product> addProduct(Product product){
         productRepository.save(product);
+        return productRepository.findAll();
+    }
+
+    public Product updateProduct(Long id, Product product){
+
+
+        for(Product existingProduct : productRepository){
+            if (existingProduct.getId() == id) {
+                existingProduct.setProductName(product.getProductName());
+                existingProduct.setProductDescription(product.getProductDescription());
+                existingProduct.setProductPrice(product.getProductPrice());
+                //return ResponseEntity.ok(existingProduct);
+                return existingProduct;
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        productRepository.(product);
         return productRepository.findAll();
     }
 }
